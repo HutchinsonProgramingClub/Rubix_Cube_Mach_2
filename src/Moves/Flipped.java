662ec7b038a8056913404a577ffd278f4c915eb6
Cole Rahne <colerahne1@student.isd423.org>
2018-05-14 14:00:08 -0500

    Added Flipped.java and Spin.java

---
+++ src/Moves/Flipped.java
@@ -0,0 +1,15 @@
+package Moves;
+
+public class Flipped {
+	
+	public static void Flipped() {
+		
+	}
+
+	public static void flipBackward() {
+		
+	}
+	public static void flipForward() {
+		
+	}
+}
---
+++ src/Moves/Spin.java
@@ -0,0 +1,31 @@
+package Moves;
+
+public class Spin {
+	public static void Spin() {
+
+	}
+	public static void Spinfull() {
+		
+	}
+	public static void Spin75() {
+		
+	}
+	public static void Spin50() {
+		
+	}
+	public static void Spin25() {
+		
+	}
+	public static void CounterSpinFull() {
+		
+	}
+	public static void CounterSpin75() {
+		
+	}
+	public static void CounterSpin50() {
+		
+	}
+	public static void CounterSpin25() {
+		
+	}
+}
