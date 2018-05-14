3ab589491bd7755af36cddf8a92f12abdb9efdbb
Cole Rahne <colerahne1@student.isd423.org>
2018-05-14 13:59:37 -0500

    Finished TurnFlip.java

--- src/Subsystem/TurnFlip.java
+++ src/Subsystem/TurnFlip.java
@@ -1,5 +1,7 @@
 package Subsystem;
 
+import Moves.Flipped;
+import Moves.Spin;
 import R.Robot;
 
 public class TurnFlip {
@@ -17,84 +19,232 @@ public class TurnFlip {
 	 *ri = right prime
 	 *u = up
 	 *ui = up prime
+	 *flip = flip forward
+	 *flipi = flip backward
+	 *quarterspin = quarter spin of the cube clockwise
+	 *quarterspini = quarter spin of the cube counterclockwise
+	 *threequarterspin = 3/4 spin of the cube clockwise
+	 *threequarterspini = 3/4 spin of the cube counterclockwise
+	 *halfspin = 1/2 spin of the cube clockwise
+	 *halfspini = 1/2 spin of the cube counterclockwise
+	 *spin = whole spin of the cube clockwise
+	 *spini = whole spin of the cube counterclockwise
+	 *flip = 1 flipper rooney dooney of the cube
+	 *flipi = flip that cube backwards man!!
 	 *</pre>
-	 * @param Move(all lowercase)
+	 * @param Move(all lowercase) = what move you want to do(look key above!!)
+	 * @param NumMoves = How many times you want the moves to run
 	 */
-	public static void TurnFlip(String Move) {
+	public static void TurnFlip(String Move, Integer NumMoves) {
 		if(Move == "di") {
-			DownPrime();
+			DownPrime(NumMoves);
 		}
 		else if(Move == "d") {
-			Down();
+			Down(NumMoves);
 		}
 		else if(Move == "b") {
-			Back();
+			Back(NumMoves);
 		}
 		else if(Move == "bi") {
-			BackPrime();
+			BackPrime(NumMoves);
 		}
 		else if(Move == "f") {
-			Front();
+			Front(NumMoves);
 		}
 		else if (Move == "fi") {
-			FrontPrime();
+			FrontPrime(NumMoves);
 		}
 		else if (Move == "l") {
-			Left();
+			Left(NumMoves);
 		}
 		else if (Move == "li") {
-			LeftPrime();
+			LeftPrime(NumMoves);
 		}
 		else if (Move == "r") {
-			Right();
+			Right(NumMoves);
 		}
 		else if (Move == "ri") {
-			RightPrime();
+			RightPrime(NumMoves);
 		}
 		else if (Move == "u") {
-			Up();
+			Up(NumMoves);
 		}
 		else if(Move == "ui") {
-			UpPrime();
+			UpPrime(NumMoves);
+		}
+		else if(Move == "flipi") {
+			flipBackward(NumMoves);
+		}
+		else if(Move == "quaterspin") {
+			quarterspin(NumMoves);
+		}
+		else if(Move == "quarterspini") {
+			quarterspini(NumMoves);
+		}
+		else if(Move == "threequarterspin") {
+			threequarterspin(NumMoves);
+		}
+		else if(Move == "threequarterspini") {
+			threequarterspini(NumMoves);
+		}
+		else if(Move == "halfspin") {
+			halfspin(NumMoves);
+		}
+		else if(Move == "halfspini") {
+			halfspini(NumMoves);
+		}
+		else if(Move == "spin") {
+			fullspin(NumMoves);
+		}
+		else if(Move == "spini") {
+			fullspini(NumMoves);
+		}
+		else if(Move == "flip") {
+			flip(NumMoves);
+		}
+		else if(Move == "flipi") {
+			flipi(NumMoves);
 		}
 		else {
 			System.out.println("Error: Your code with the Moves doesn't work (Look in TurnFlip.java)");
 		}
 	}
-	private static void Down() {
-		Robot.Down();
+	private static void Down(Integer NumMoves) {
+		while(NumMoves > 0){
+            Robot.Down();
+            NumMoves--;
+       }
 	}
-	private static void DownPrime() {
-		Robot.DownPrime();
+	private static void DownPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.DownPrime();
+            NumMoves--;
+       }
 	}
-	private static void Back() {
-		Robot.Back();
+	private static void Back(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.Back();
+            NumMoves--;
+       }
 	}
-	private static void BackPrime() {
-		Robot.BackPrime();
+	private static void BackPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.BackPrime();
+            NumMoves--;
+       }
 	}
-	private static void Front() {
-		Robot.Front();
+	private static void Front(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.Front();
+            NumMoves--;
+       }
 	}
-	private static void FrontPrime() {
-		Robot.FrontPrime();
+	private static void FrontPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.FrontPrime();
+			NumMoves--;
+       }
 	}
-	private static void Left() {
-		Robot.Left();
+	private static void Left(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.Left();
+            NumMoves--;
+       }
 	}
-	private static void LeftPrime() {
-		Robot.LeftPrime();
+	private static void LeftPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.LeftPrime();
+            NumMoves--;
+       }
 	}
-	private static void Right() {
-		Robot.Right();
+	private static void Right(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.Right();
+            NumMoves--;
+       }
 	}
-	private static void RightPrime() {
-		Robot.RightPrime();
+	private static void RightPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.RightPrime();
+            NumMoves--;
+       }
 	}
-	private static void Up() {
-		Robot.Up();
+	private static void Up(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.Up();
+            NumMoves--;
+       }
 	}
-	private static void UpPrime() {
-		Robot.UpPrime();
+	private static void UpPrime(Integer NumMoves) {
+		while(NumMoves > 0){
+			Robot.UpPrime();
+            NumMoves--;
+       }
+	}
+	private static void flipBackward(Integer NumMoves) {
+		while(NumMoves > 0){
+			Flipped.flipBackward();
+			NumMoves--;
+       }
+	}
+	private static void threequarterspin(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.Spin75();
+			NumMoves--;
+       }
+	}
+	private static void threequarterspini(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.CounterSpin75();
+			NumMoves--;
+       }
+	}
+	private static void quarterspin(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.Spin50();
+			NumMoves--;
+       }
+	}
+	private static void quarterspini(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.CounterSpin75();
+			NumMoves--;
+       }
+	}
+	private static void halfspin(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.Spin50();
+			NumMoves--;
+       }
+	}
+	private static void halfspini(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.CounterSpin50();
+			NumMoves--;
+       }
+	}
+	private static void fullspin(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.Spinfull();
+			NumMoves--;
+       }
+	}
+	private static void fullspini(Integer NumMoves) {
+		while(NumMoves > 0){
+			Spin.CounterSpinFull();
+			NumMoves--;
+       }
+	}
+	private static void flip(Integer NumMoves) {
+		while(NumMoves > 0){
+			Flipped.flipForward();
+			NumMoves--;
+       }
+	}
+	private static void flipi(Integer NumMoves) {
+		while(NumMoves > 0){
+			Flipped.flipBackward();
+			NumMoves--;
+       }
 	}
 }
