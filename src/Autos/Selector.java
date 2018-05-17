94f1f69ce5dfeb1da1d255705bafd15950467522
Cole Rahne <colerahne1@student.isd423.org>
2018-05-17 11:22:34 -0500

    Life sucks

--- src/Autos/Selector.java
+++ src/Autos/Selector.java
@@ -1,9 +1,14 @@
 package Autos;
+
+import Subsystem.TurnFlip;
+
 /**
  * Selects the Autos to run (solves cube)
  *
  */
 public class Selector {
 	
-
+	public Selector() {
+		TurnFlip.TurnFlip("flip", 5);
+	}
 }
---
+++ src/Autos/findCorner.java
@@ -0,0 +1,9 @@
+package Autos;
+/**
+ * WE NEED TO MAKE THIS WORK WITH TESTING
+ * @author Cole
+ *
+ */
+public class findCorner {
+
+}
